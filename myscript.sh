d63c23d0a017cb930ef79f07dbdf025592cf047b
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-03-10 16:09:54 +0800

    Update

--- myscript.sh
+++ myscript.sh
@@ -6,5 +6,4 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=32000
 
-srun /home/dobreschkow/surfsuite/surfsuite makeall -simulation L210_N1024-Hydro3D-hyades -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile3D.txt
-srun /home/dobreschkow/surfsuite/surfsuite makeall -simulation L210_N1024-Hydro6D-hyades -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile6D.txt
+srun /home/dobreschkow/surfsuite/surfsuite makehalos -simulation L210_N1024-Hydro6Dsubs-hyades -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile6Dsubs.txt
