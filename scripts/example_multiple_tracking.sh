35ed31c7f03c235fcf917b0d2f0cb8dbe7a4e8ff
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-03-13 09:36:54 +0800

    Update

--- scripts/example_multiple_tracking.sh
+++ scripts/example_multiple_tracking.sh
@@ -8,6 +8,6 @@
 
 for halo in {79085,455803,223918,672740,1118933}
 do
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo $halo -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_79085.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_$halo.txt &
+srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo $halo -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_79085.hdf -from 100 -to 199 -logfile /home/dobreschkow/surfsuite/log_track_$halo.txt &
 done
 wait
