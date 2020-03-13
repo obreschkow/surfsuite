300161d7cbf3d7596c97192a67001823ec2c0069
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-03-13 08:32:03 +0800

    Update

--- scripts/example_sort_all_snapshots.sh
+++ scripts/example_sort_all_snapshots.sh
@@ -10,5 +10,5 @@ module load gfortran/6.3.0 hdf5/1.10.2
 
 for sn in {0..199}
 do
-   srun -n=1 /home/dobreschkow/surfsuite/surfsuite sortparticles -simulation L210_N1024-Hydro3D-hyades -snapshot $sn -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile3D.txt
+   srun -n 1 /home/dobreschkow/surfsuite/surfsuite sortparticles -simulation L210_N1024-Hydro3D-hyades -snapshot $sn -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile3D.txt
 done
