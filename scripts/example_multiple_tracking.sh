409168bf1ef26ae5be0cda43533d131e4ecc4cd0
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-03-13 09:21:43 +0800

    Update

--- scripts/example_multiple_tracking.sh
+++ scripts/example_multiple_tracking.sh
@@ -2,12 +2,12 @@
 #
 #SBATCH --job-name=surfsuite
 #
-#SBATCH --ntasks=5
+#SBATCH --ntasks=8
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=16000
 
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo 79085 -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_79085.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_79085.txt
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo 455803 -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_455803.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_455803.txt
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo 223918 -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_223918.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_223918.txt
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo 672740 -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_672740.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_672740.txt
-srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo 1118933 -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_1118933.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_1118933.txt
+for halo in {79085,455803,223918,672740,1118933}
+do
+srun -n 1 /home/dobreschkow/surfsuite/surfsuite trackhalo $halo -simulation L210_N1024-Hydro3D-hyades -snapshot 199 -subhalos 1 -center 1 -parameterfile /home/dobreschkow/surfsuite/parameters.txt -outputfile /home/dobreschkow/track_halo_79085.hdf -from 100 -to 199 -logfile /home/dobreschkow/log_$halo.txt &
+done
+wait
--- scripts/example_single_task.sh
+++ scripts/example_single_task.sh
@@ -6,4 +6,4 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=32000
 
-srun /home/dobreschkow/surfsuite/surfsuite makehalos -simulation L210_N1024-Hydro6Dsubs-hyades -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/logfile6Dsubs.txt
+srun /home/dobreschkow/surfsuite/surfsuite makehalos -simulation L210_N1024-Hydro6Dsubs-hyades -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/log.txt
