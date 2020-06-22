cc413daa96aa20f688df2b5b037f9852bd9eb33f
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-06-22 11:29:27 +0800

    Update

--- scripts/example_makehalos.sh
+++ scripts/example_makehalos.sh
@@ -10,6 +10,6 @@ module load gfortran/6.3.0 hdf5/1.10.2
 
 for sn in {131,156,199}
 do
-   srun -n 1 /home/dobreschkow/surfsuite/surfsuite makehalos -parameterset L210_N1024-new-hyades -snapshot $sn -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/log.txt &
+   srun -n 1 /home/dobreschkow/surfsuite/surfsuite makehalos -parameterset L210_N1024-new-hyades -snapshot $sn -parameterfile /home/dobreschkow/surfsuite/parameters.txt -logfile /home/dobreschkow/surfsuite/log$sn.txt &
 done
 wait
